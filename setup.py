294dabae08c7e2dce7607072a4bbcb7550a0f2d9
0xnrous <0xnrous@gmail.com>
2024-01-19 21:28:35 +0200

    v0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it's ITI project that we develop a data-driven solution for IT institue to understand relationships between the jobs and the technologies in market',
+    author='0xnrous',
+    license='MIT',
+)
